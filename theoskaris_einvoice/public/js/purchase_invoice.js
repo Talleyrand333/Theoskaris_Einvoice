92392cb7e23bc6f31cfd6d0f2873bd32e0c93a38
Theoskaris <hello@theoskaris.com>
2026-07-24 00:48:00 +0200

    fix: show FIRS section always on Purchase Invoice, hide only status fields
    Section Break can't be toggled off since it hides the checkbox too.
    Only hide the read-only status/IRN fields until Submit to NRS is checked.

--- theoskaris_einvoice/public/js/purchase_invoice.js
+++ theoskaris_einvoice/public/js/purchase_invoice.js
@@ -2,21 +2,25 @@
 
 frappe.ui.form.on("Purchase Invoice", {
 	refresh(frm) {
-		toggle_firs_tab(frm);
+		toggle_firs_fields(frm);
 	},
 
 	custom_submit_to_nrs(frm) {
-		toggle_firs_tab(frm);
+		toggle_firs_fields(frm);
 	},
 
 	is_return(frm) {
-		toggle_firs_tab(frm);
+		toggle_firs_fields(frm);
 	},
 });
 
-function toggle_firs_tab(frm) {
-	const show = frm.doc.custom_submit_to_nrs || cint(frm.doc.is_return);
-	frm.toggle_display("custom_firs_tab", show);
+function toggle_firs_fields(frm) {
+	// Always show the section break and checkbox.
+	// Hide status/IRN fields until Submit to NRS is checked.
+	const show_status = frm.doc.custom_submit_to_nrs || cint(frm.doc.is_return);
+	const fields = ["custom_nrs_status", "custom_nrs_irn", "custom_nrs_qr_code",
+		"custom_nrs_qr_code_url", "custom_nrs_response", "custom_nrs_datetime"];
+	fields.forEach(f => frm.toggle_display(f, show_status));
 	if (frm.doc.custom_nrs_irn) {
 		frm.set_df_property("custom_submit_to_nrs", "read_only", 1);
 	}
